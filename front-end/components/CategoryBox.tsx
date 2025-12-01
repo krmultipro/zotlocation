e0bfb452c72c13d875afc201cd916f25464d372c
chloebaisse1 <baissechloe@gmail.com>
2025-12-01 18:28:03 +0400

    modifications

--- front-end/components/CategoryBox.tsx
+++ front-end/components/CategoryBox.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useRouter, useSearchParams } from "next/navigation"
 import qs from "query-string"
 import { useCallback } from "react"
