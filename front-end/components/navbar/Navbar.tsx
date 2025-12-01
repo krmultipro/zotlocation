02804c7e39418a49e8913c7d20b4e510ddb1f913
chloebaisse1 <baissechloe@gmail.com>
2025-12-01 20:02:45 +0400

    modif

--- front-end/components/navbar/Navbar.tsx
+++ front-end/components/navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client"
 import Container from "../Container"
-import Categories from "./Categories"
+
 import Logo from "./Logo"
 import Search from "./Search"
 import UserMenu from "./UserMenu"
@@ -17,7 +17,6 @@ const Navbar = () => {
           </div>
         </Container>
       </div>
-      <Categories />
     </div>
   )
 }
