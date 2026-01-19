94d661be598da16636120cbc35c8b27b018666b2
Baisse Chloe <baissechloe@gmail.com>
2026-01-19 14:02:01 +0400

    suppression de l'onglet mon compte quand l'utilisateur n'est pas connecté, modification du bouton se connecter en connexion

--- front-end/components/navbar/Logo.tsx
+++ front-end/components/navbar/Logo.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import Image from "next/image"
-import { useRouter } from "next/navigation" // attention ici a l'importation, ce n'est pas "next/router" mais bien "next/navigation"
+import Image from "next/image";
+import { useRouter } from "next/navigation"; // attention ici a l'importation, ce n'est pas "next/router" mais bien "next/navigation"
 
 const Logo = () => {
   const router = useRouter()
